7e67df0636530c28f5a477b6c5f69a5a7f6f1b44
McAlvaro <mc.alv641@gmail.com>
2024-04-30 18:37:22 -0400

    Added partial changes java-debugger

---
+++ lua/mcalvaro/dap/configs/java.lua
@@ -0,0 +1,27 @@
+local dap = require("dap")
+
+dap.adapters.java = {
+	type = "server",
+	host = "127.0.0.1",
+	port = 5005,
+}
+
+dap.configurations.java = {
+	{
+		type = "java",
+		request = "launch",
+		name = "Launch file",
+		mainClass = "${file}",
+		args = {},
+		cwd = "${workspaceFolder}",
+		console = "integratedTerminal",
+		stopOnEntry = false,
+	},
+	{
+		type = "java",
+		name = "Debug (attach)",
+		request = "attach",
+		hostName = "127.0.0.1",
+		port = 5005,
+	},
+}
--- lua/mcalvaro/mason/setups.lua
+++ lua/mcalvaro/mason/setups.lua
@@ -97,6 +97,116 @@ return setmetatable({
 			},
 		}
 	end,
+	jdtls = function()
+		-- Setup Workspace
+		local home = os.getenv("HOME")
+		local workspace_path = home .. "/.local/share/nvim/jdtls_workspace/"
+		local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+		local workspace_dir = workspace_path .. project_name
+
+		local os_config = "linux"
+		if vim.fn.has("mac") == 1 then
+			os_config = "mac"
+		end
+
+		-- vim.list_extend(
+			-- bundles,
+			-- vim.split(vim.fn.glob(mason_path .. "packages/java-test/extension/server/*.jar",1), "\n")
+		-- )
+		-- vim.list_extend(
+			-- bundles,
+			-- vim.split(
+			-- 	vim.fn.glob(
+			-- 		mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", 1
+			-- 	),
+			-- 	"\n"
+			-- )
+		-- )
+		-- -- vim.builtin.dap.active = true
+
+		return {
+			filetypes = {
+				"java",
+			},
+			cmd = {
+				"/usr/lib/jvm/java-1.21.0-openjdk-amd64/bin/java",
+				"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+				"-Dosgi.bundles.defaultStartLevel=4",
+				"-Declipse.product=org.eclipse.jdt.ls.core.product",
+				"-Dlog.protocol=true",
+				"-Dlog.level=ALL",
+				"-Xms1g",
+				"--add-modules=ALL-SYSTEM",
+				"--add-opens",
+				"java.base/java.util=ALL-UNNAMED",
+				"--add-opens",
+				"java.base/java.lang=ALL-UNNAMED",
+				"-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+				"-jar",
+				vim.fn.glob(
+					home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"
+				),
+				"-configuration",
+				home .. "/.local/share/nvim/mason/packages/jdtls/config_" .. os_config,
+				"-data",
+				workspace_dir,
+			},
+			root_dir = require("lspconfig.server_configurations.jdtls").default_config.root_dir,
+			settings = {
+				java = {
+					eclipse = {
+						downloadSources = true,
+					},
+					configuration = {
+						updateBuildConfiguration = "interactive",
+						runtimes = {
+							{
+								name = "JavaSE-21",
+								path = "/usr/lib/jvm/java-1.21.0-openjdk-amd64",
+							},
+						},
+					},
+					maven = {
+						downloadSources = true,
+					},
+					implementationsCodeLens = {
+						enabled = true,
+					},
+					referencesCodeLens = {
+						enabled = true,
+					},
+					references = {
+						includeDecompiledSources = true,
+					},
+					inlayHints = {
+						parameterNames = {
+							enabled = "all", -- literals, all, none
+						},
+					},
+					format = {
+						enabled = false,
+					},
+				},
+				signatureHelp = { enabled = true },
+			},
+			init_options = {
+				bundles = {
+                    "/home/alvaro/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-0.50.0.jar",
+                    				vim.fn.glob("/home/alvaro/.local/share/nvim/mason/packages/java-test/extension/server/com.microsoft.java.debug.plugin-*.jar", 1)
+
+                },
+			},
+			on_attach = function(client, bufnr)
+				local _, _ = pcall(vim.lsp.codelens.refresh)
+				require("jdtls.dap").setup_dap_main_class_configs()
+				require("vim.lsp").on_attach(client, bufnr)
+				-- local status_ok, jdtls_dap = pcall(require, "jdtls.dap")
+				-- if status_ok then
+					-- jdtls_dap.setup_dap_main_class_configs()
+				-- end
+			end,
+		}
+	end,
 }, {
 	__index = function()
 		return function()
