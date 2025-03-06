1e9d9ad9c7596d1e0ea27a27b324caf63d68c4f9
McAlvaro <mc.alv641@gmail.com>
2025-03-06 18:28:14 -0400

    Minor Fix

--- lua/mcalvaro/utils/create_class.lua
+++ lua/mcalvaro/utils/create_class.lua
@@ -1,9 +1,13 @@
-local lib = require("nvim-tree.lib")
+local core = require("nvim-tree.core")
 local actions = require("mcalvaro.utils.nvim-tree.actions.create_java_class")
 
 function wrap_node_or_nil(fn, class_type)
 	return function(node, ...)
-		node = node or lib.get_node_at_cursor()
+		local explorer = core.get_explorer()
+		if not explorer then
+			return
+		end
+		node = node or explorer:get_node_at_cursor()
 		fn(node, class_type, ...)
 	end
 end
@@ -16,19 +20,16 @@ end
 local class_types = { "Class", "Interface", "Enum", "Record" }
 
 function create_java_class()
-
 	vim.ui.select(class_types, {
 
 		prompt = "New Java Class",
 		telescope = require("telescope.themes").get_dropdown(),
-
 	}, function(selected)
+		if not selected then
+			return
+		end
 
-            if not selected then
-                return
-            end
-
-            local wraped_action = wrap_node_or_nil(actions.fn, selected)
-            wraped_action()
-        end)
+		local wraped_action = wrap_node_or_nil(actions.fn, selected)
+		wraped_action()
+	end)
 end
