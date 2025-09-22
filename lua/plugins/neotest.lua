f85811fbedf768123112d6e73e2bf297b58fc3cf
McAlvaro <mc.alv641@gmail.com>
2025-09-22 18:53:48 -0400

    fix: minor fix neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -28,14 +28,50 @@ return {
 				require("neotest-rust"),
 				require("neotest-plenary"),
 				require("neotest-phpunit")({
-					filter_dirs = { "vendor" },
+					phpunit_cmd = function()
+						return "vendor/bin/phpunit" -- for `dap` strategy then it must return string (table values will cause validation error)
+					end,
+					root_files = { "composer.json", "phpunit.xml", ".gitignore" },
+					filter_dirs = { ".git", "node_modules", "vendor" },
+					env = {}, -- for example {XDEBUG_CONFIG = 'idekey=neotest'}
+					dap = nil, -- to configure `dap` strategy put single element from `dap.configurations.php`
 				}),
 				require("neotest-java"),
+				require("neotest-python")({
+					-- Extra arguments for nvim-dap configuration
+					-- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+					dap = { justMyCode = false },
+					-- Command line arguments for runner
+					-- Can also be a function to return dynamic values
+					args = { "--log-level", "DEBUG" },
+					-- Runner to use. Will use pytest if available by default.
+					-- Can be a function to return dynamic value.
+					runner = "pytest",
+					-- Custom python path for the runner.
+					-- Can be a string or a list of strings.
+					-- Can also be a function to return dynamic value.
+					-- If not provided, the path will be inferred by checking for
+					-- virtual envs in the local directory and for Pipenev/Poetry configs
+					python = ".venv/bin/python3",
+					-- Returns if a given file path is a test file.
+					-- NB: This function is called a lot so don't perform any heavy tasks within it.
+					-- !!EXPERIMENTAL!! Enable shelling out to `pytest` to discover test
+					-- instances for files containing a parametrize mark (default: false)
+					pytest_discover_instances = true,
+					-- is_test_file = function(file_path)
+					-- 	return file_path:match("test_.+%.py") ~= nil or file_path:match("_test%.py") ~= nil
+					-- end,
+				}),
 				require("neotest-vim-test")({
 					filter_dirs = { "vendor" },
 					ignore_file_types = { "go", "lua", "rust", "php" },
 				}),
 				-- require("laravel.neotest"),
+			},
+			-- overseer.nvim
+			consumers = {
+				-- overseer = require("neotest.consumers.overseer"),
+				-- neotest_consumers = require("neotest-consumers").consumers
 			},
 			overseer = {
 				enabled = true,
@@ -59,6 +95,7 @@ return {
 			name = "neotest",
 			hint = hint,
 			mode = "n",
+			foreign_keys = "warn",
 			config = {
 				color = "teal",
 				invoke_on_body = true,
@@ -66,7 +103,7 @@ return {
 					type = "window",
 					position = "bottom",
 					float_opts = {
-                        border = "rounded",
+						border = "rounded",
 						style = "minimal",
 						focusable = false,
 						noautocmd = true,
