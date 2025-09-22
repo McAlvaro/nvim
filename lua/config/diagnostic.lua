810d49a5fc6309a258a5d16babf5f198ae5a8e1f
McAlvaro <mc.alv641@gmail.com>
2025-09-22 18:39:33 -0400

    feat(blade): add blade template syntax highlighting

--- lua/config/diagnostic.lua
+++ lua/config/diagnostic.lua
@@ -7,3 +7,9 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
+
+vim.filetype.add({
+    pattern = {
+        ['.*%.blade%.php'] = 'blade',
+    }
+})
---
+++ lua/plugins/blade-nav.lua
@@ -0,0 +1,13 @@
+return {
+	"ricardoramirezr/blade-nav.nvim",
+	dependencies = { -- totally optional
+		"hrsh7th/nvim-cmp", -- if using nvim-cmp
+		-- { "ms-jpq/coq_nvim", branch = "coq" }, -- if using coq
+		-- "saghen/blink.cmp", -- if using blink.cmp
+	},
+	ft = { "blade", "php" }, -- optional, improves startup time
+	opts = {
+		-- This applies for nvim-cmp and coq, for blink refer to the configuration of this plugin
+		close_tag_on_complete = false, -- default: true
+	},
+}
