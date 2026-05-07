aacff994e307a75753612fb697ba2cc5afeaaccf
McAlvaro <mc.alv641@gmail.com>
2026-05-07 19:44:32 -0400

    refactor: conditional adapter loading in neotest config

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -22,57 +22,79 @@ return {
 
 	config = function()
 		local neotest = require("neotest")
-		neotest.setup({
-			adapters = {
-				require("neotest-go"),
-				require("neotest-rust"),
-				require("neotest-plenary"),
+
+		local function has_root_file(files)
+			local cwd = vim.fn.getcwd()
+			for _, file in ipairs(files) do
+				if vim.loop.fs_stat(cwd .. "/" .. file) then
+					return true
+				end
+			end
+			return false
+		end
+
+		local adapters = {
+			require("neotest-plenary"),
+		}
+
+		-- 🐘 PHP
+		if has_root_file({ "composer.json", "phpunit.xml", "pest.php" }) then
+			table.insert(
+				adapters,
 				require("neotest-phpunit")({
 					phpunit_cmd = function()
-						return "vendor/bin/phpunit" -- for `dap` strategy then it must return string (table values will cause validation error)
+						return "vendor/bin/phpunit"
 					end,
 					root_files = { "composer.json", "phpunit.xml", ".gitignore" },
 					filter_dirs = { ".git", "node_modules", "vendor" },
-					env = {}, -- for example {XDEBUG_CONFIG = 'idekey=neotest'}
-					dap = nil, -- to configure `dap` strategy put single element from `dap.configurations.php`
-				}),
-				require("neotest-java"),
+					env = {},
+					dap = nil,
+				})
+			)
+		end
+
+		-- ☕ Java
+		if has_root_file({ "pom.xml", "build.gradle", ".project" }) then
+			table.insert(adapters, require("neotest-java"))
+		end
+
+		-- 🐹 Go
+		if has_root_file({ "go.mod" }) then
+			table.insert(adapters, require("neotest-go"))
+		end
+
+		-- 🦀 Rust
+		if has_root_file({ "Cargo.toml" }) then
+			table.insert(adapters, require("neotest-rust"))
+		end
+
+		-- 🐍 Python
+		if has_root_file({ "requirements.txt", "pyproject.toml", "setup.py", "manage.py" }) then
+			table.insert(
+				adapters,
 				require("neotest-python")({
-					-- Extra arguments for nvim-dap configuration
-					-- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
 					dap = { justMyCode = false },
-					-- Command line arguments for runner
-					-- Can also be a function to return dynamic values
 					args = { "--log-level", "DEBUG" },
-					-- Runner to use. Will use pytest if available by default.
-					-- Can be a function to return dynamic value.
 					runner = "pytest",
-					-- Custom python path for the runner.
-					-- Can be a string or a list of strings.
-					-- Can also be a function to return dynamic value.
-					-- If not provided, the path will be inferred by checking for
-					-- virtual envs in the local directory and for Pipenev/Poetry configs
 					python = ".venv/bin/python3",
-					-- Returns if a given file path is a test file.
-					-- NB: This function is called a lot so don't perform any heavy tasks within it.
-					-- !!EXPERIMENTAL!! Enable shelling out to `pytest` to discover test
-					-- instances for files containing a parametrize mark (default: false)
 					pytest_discover_instances = true,
-					-- is_test_file = function(file_path)
-					-- 	return file_path:match("test_.+%.py") ~= nil or file_path:match("_test%.py") ~= nil
-					-- end,
-				}),
-				require("neotest-vim-test")({
-					filter_dirs = { "vendor" },
-					ignore_file_types = { "go", "lua", "rust", "php" },
-				}),
-				-- require("laravel.neotest"),
-			},
+				})
+			)
+		end
+
+		-- 🏁 Fallback / Generic
+		table.insert(
+			adapters,
+			require("neotest-vim-test")({
+				filter_dirs = { "vendor" },
+				ignore_file_types = { "go", "lua", "rust", "php" },
+			})
+		)
+
+		neotest.setup({
+			adapters = adapters,
 			-- overseer.nvim
-			consumers = {
-				-- overseer = require("neotest.consumers.overseer"),
-				-- neotest_consumers = require("neotest-consumers").consumers
-			},
+			consumers = {},
 			overseer = {
 				enabled = true,
 				force_default = true,
