02d58234050a23310fdfbadb49684d9659f1d594
Alvaro <alvaro@192.168.2.19>
2026-08-17 15:53:59 -0400

    chore: remove unused plugins and optimize config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,52 +1,43 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
 	build = ":TSUpdate",
+	lazy = false,
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "gnn",
-					node_incremental = "grn",
-					scope_incremental = "grc",
-					node_decremental = "grm",
-				},
-			},
-			indent = {
-				enable = true,
-				disable = { "python" },
-			},
-			ensure_installed = {
-				"javascript", -- Agrega aqu√≠ los lenguajes que deseas mantener activos
-				"html",
-				"bash",
-				"comment",
-				"css",
-				"csv",
-				"dockerfile",
-				"git_config",
-				"gitattributes",
-				"gitcommit",
-				"gitignore",
-				"json",
-				"jsonc",
-				"lua",
-				"markdown",
-				"php",
-				"phpdoc",
-				"python",
-				"scss",
-				"sql",
-				"tsx",
-				"typescript",
-				"vue",
-				"xml",
-				"yaml",
-			},
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "php" }, -- Reemplazamos <filetype> por 'php'
+			callback = function()
+				vim.treesitter.start()
+			end,
 		})
 	end,
+	opts = {
+		ensure_installed = {
+			"javascript", -- Agrega aquí los lenguajes que deseas mantener activos
+			"html",
+			"bash",
+			"comment",
+			"css",
+			"csv",
+			"dockerfile",
+			"git_config",
+			"gitattributes",
+			"gitcommit",
+			"gitignore",
+			"json",
+			"jsonc",
+			"lua",
+			"markdown",
+			"php",
+			"phpdoc",
+			"python",
+			"scss",
+			"sql",
+			"tsx",
+			"typescript",
+			"vue",
+			"xml",
+			"yaml",
+		},
+	},
 }
